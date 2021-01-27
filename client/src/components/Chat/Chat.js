28b65f2845185fe5c3a56154e62a6b14621f9f50
Anastasia Volosevych <volosevichh@gmail.com>
2021-01-27 13:09:04 -0700

    was added img for online status and close img for closing chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+import InfoBar from '../InfoBar/InfoBar';
+import './Chat.css';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContaier">
+            <div className="container">
+                <InfoBar/>
+                {/* <input value={message}
+                onChange={(event) => setMessages(event.target.value)}
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/> */}
+            </div>
+        </div>
     )
 }
 
